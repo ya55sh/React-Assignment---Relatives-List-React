b3ab9f3546e05d2ccd257d2476296cb9101b2c30
Yash Bahuguna <yashbahuguna10@gmail.com>
2020-11-23 22:41:25 +0530

    keys and stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+const relativeList = ["Yash", "Raaj", "Films"];
 
+// const numbers = [1, 2, 3, 4, 5];
+let counter = 1;
+const listItems = relativeList.map((rlist,index) => (
+  <li key={"relativeListItem" + (index+1)}> {rlist}</li>
+));
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return <div id="main"><ol>{listItems}</ol></div>;
+  }
 }
-
 
 export default App;
