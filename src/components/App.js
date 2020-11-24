06577b8f980a8918aafa579f4eafe462cdac1c42
Yash Bahuguna <yashbahuguna10@gmail.com>
2020-11-24 18:10:11 +0530

    re built return function

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,20 @@ import "../styles/App.css";
 const relativeList = ["Yash", "Raaj", "Films"];
 
 // const numbers = [1, 2, 3, 4, 5];
-let counter = 1;
-const listItems = relativeList.map((rlist,index) => (
-  <li key={"relativeListItem" + (index+1)}> {rlist}</li>
-));
+// const listItems = relativeList.map((rlist, index) => (
+//   <li key={"relativeListItem" + (index + 1)}> {rlist}</li>
+// ));
 class App extends Component {
   render() {
-    return <div id="main"><ol>{listItems}</ol></div>;
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          {relativeList.map((relative,idx)=>(
+            <li key={"relativeListItem"+(idx+1)}>{relative}</li>
+          ))}
+        </ol>
+      </div>
+    );
   }
 }
 
